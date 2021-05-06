1fa069bf15611b3ff63157b2afad8654f668f409
higashionna <higashionna@everlifegroup.com>
2021-05-06 13:31:54 +0900

    線の太さ調整

--- pages/components/BoderCount.js
+++ pages/components/BoderCount.js
@@ -3,12 +3,12 @@ export const BoderCount = ({ playerCount }) => {
     return (
         <tr className="w-full">
             <th colSpan="1"></th>
-            <th className="border-l border-r border-gray-700 h-8" colSpan="2"></th>
+            <th className="border-l-4 border-r-4 border-gray-700 h-8" colSpan="2"></th>
             {
                 (() => {
                     const thTagCreate = [];
                     for (let i = 0; i < playerCounts; i++) {
-                        thTagCreate.push(<th className="border-r border-gray-700 h-8" colSpan="2"></th>)
+                        thTagCreate.push(<th className="border-r-4 border-gray-700 h-8" colSpan="2"></th>)
                     }
                     for (let i = playerCounts; i < 8; i++) {
                         thTagCreate.push(<th colSpan="2"></th>)
