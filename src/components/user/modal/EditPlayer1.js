2e19cf619fcf3acd6fa85e0033b808026f16fd06
higashionna <higashionna@everlifegroup.com>
2021-05-17 22:33:00 +0900

    iPhoneバグ修正したい

--- src/components/user/modal/EditPlayer1.js
+++ src/components/user/modal/EditPlayer1.js
@@ -51,5 +51,5 @@ export const EditPlayer1 = () => {
         </>
     ), [value])
 
-    return <p className="bg-primary text-white cursor-pointer writing-mode-vertical mx-auto py-6 px-1 border border-gray-200　min-h-full" onClick={showModal}>選択する</p>
+    return <p className="bg-primary text-white cursor-pointer writing-mode-vertical mx-auto py-6 px-1 border border-gray-200" onClick={showModal}>選択する</p>
 }
