5071b510b5defef36da482f0a862d18c730c7e57
higashionna <higashionna@everlifegroup.com>
2021-05-20 09:21:29 +0900

    微調整

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -9,6 +9,9 @@ const Footer = () => {
   const url = 'https://amida.vercel.app' + router.asPath
   return (
     <footer className="fixed bottom-0 w-full text-right text-gray-700 text-sm bg-gray-200">
+      <small className="mr-8">
+        <a>&copy; {new Date().getFullYear()} Imoto All Rights Reserved.</a>
+      </small>
       <div className="">
         <button className="bg-gray-700  hover:bg-gray-500 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2" onClick={() => router.push("/")}><Icon icon={faHome} /> あみだ作成ページに移動する</button>
         <CopyToClipboard
@@ -18,9 +21,7 @@ const Footer = () => {
           <button className="bg-blue-500  hover:bg-blue-300 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2"><Icon icon={faCopy} /> クリップボードにURLをコピーする</button>
         </CopyToClipboard>
       </div>
-      <small className="mr-8">
-          <a>&copy; {new Date().getFullYear()} Imoto All Rights Reserved.</a>
-      </small>
+
     </footer>
   );
 };
