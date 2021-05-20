3a38a03f98e164a45d26c7471258d79d5902a82b
higashionna <higashionna@everlifegroup.com>
2021-05-20 22:32:13 +0900

    調整

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -7,15 +7,20 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   const router = useRouter()
   const url = 'https://amida.vercel.app' + router.asPath
+  const urlChack = router.asPath == "/"
+  console.log(urlChack)
   return (
     <footer className="fixed bottom-0 w-full text-right text-gray-700 text-sm bg-gray-200 h-12">
-      <div className="">
-        <button className="bg-gray-700  hover:bg-gray-500 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2 h-12" onClick={() => router.push("/")}><Icon icon={faHome} /> あみだくじ作成ページに移動</button>
+      <div>
+        {
+          (!urlChack) ? <button className="bg-gray-700  hover:bg-gray-500 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2 h-12" onClick={() => router.push("/")}><Icon icon={faHome} /> あみだくじ作成ページに移動</button>
+            : <button className="bg-gray-700  hover:bg-gray-500 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2 h-12" onClick={() => router.push("/")}><Icon icon={faHome} /> このサイトの説明</button>
+        }
         <CopyToClipboard
           text={url}
           onCopy={() => alert(`クリップボードにコピーしました！`)}
         >
-          <button className="bg-blue-500  hover:bg-blue-300 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2 h-12"><Icon icon={faCopy} /> クリップボードにURLをコピー</button>
+          <button className="bg-blue-500  hover:bg-blue-300 border-2 border-gray-200 text-white p-2 font-bold text-xs focus:outline-none w-1/2 h-12"><Icon icon={faCopy} /> このサイトのURLをコピー</button>
         </CopyToClipboard>
       </div>
 
