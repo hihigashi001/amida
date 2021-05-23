fc9baa68d6c90c91a36de44f0687aa12b5bc327b
higashionna <higashionna@everlifegroup.com>
2021-05-23 13:12:31 +0900

    SEO対策

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,8 +9,21 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <meta name="description" content="EXA" />
+          <meta charset="utf-8" />
+          <meta name="description" content="オンラインあみだくじサイトです。完全無料で使えてシンプルかつ簡単にご利用いただけます。サイト上でアミダくじを瞬時に作成してURLを共有することで公平な抽選が可能です。順番決め、幹事決め、チーム分け、グループ分け、係や役割分担、景品の抽選など使い方は工夫次第です。離れた場所にいる場合で何かを決めるのにとても役に立つサイトです。" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="favi16.png" sizes="16x16" type="image/png" />
+          <link rel="icon" href="favi32.png" sizes="32x32" type="image/png" />
+          <link rel="icon" href="favi48.png" sizes="48x48" type="image/png" />
+          <link rel="icon" href="favi62.png" sizes="62x62" type="image/png" />
+          <link rel="apple-touch-icon-precomposed" href="favi150.png" />
+          <link rel="canonical" href="https://amidakuji.net/" />
+          <meta property="og:title" content="あみだくじサイト" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="オンラインあみだくじサイトです。完全無料で使えてシンプルかつ簡単にご利用いただけます。サイト上でアミダくじを瞬時に作成してURLを共有することで公平な抽選が可能です。順番決め、幹事決め、チーム分け、グループ分け、係や役割分担、景品の抽選など使い方は工夫次第です。離れた場所にいる場合で何かを決めるのにとても役に立つサイトです。" />
+          <meta property="og:url" content="https://amidakuji.net/" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:site_name" content="完全無料シンプルあみだくじオンラインサイト" />
         </Head>
         <body>
           <Main />
