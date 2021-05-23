7ba8a43c8191f3713bf7d362a376ce63c28f2f77
higashionna <higashionna@everlifegroup.com>
2021-05-23 16:19:34 +0900

    SEOの文言修正

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
           <link rel="canonical" href="https://amidakuji.net/" />
           <meta property="og:title" content="あみだくじサイト" />
           <meta property="og:type" content="website" />
-          <meta property="og:description" content="オンラインあみだくじサイトです。完全無料で使えてシンプルかつ簡単にご利用いただけます。サイト上でアミダくじを瞬時に作成してURLを共有することで公平な抽選が可能です。順番決め、幹事決め、チーム分け、グループ分け、係や役割分担、景品の抽選など使い方は工夫次第です。離れた場所にいる場合で何かを決めるのにとても役に立つサイトです。" />
+          <meta property="og:description" content="オンラインあみだくじサイトです。完全無料で使えてシンプルかつ簡単にご利用いただけます。サイト上でアミダくじを瞬時に作成してURLを共有することで公平な抽選が可能です。順番決め、幹事決め、チーム分け、グループ分け、係や役割分担、景品の抽選など使い方は工夫次第です。離れた場所にいる場合で何かを決めるのにとても役に立つサイトです。あみだくじアプリのようなインストール作業も不要です。" />
           <meta property="og:url" content="https://amidakuji.net/" />
           <meta property="og:locale" content="ja_JP" />
           <meta property="og:site_name" content="完全無料シンプルあみだくじオンラインサイト" />
