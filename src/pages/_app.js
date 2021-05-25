b74828fa25c17a841c3c287690b51e2def1a3a24
higashionna <higashionna@everlifegroup.com>
2021-05-25 13:52:14 +0900

    パフォーマンス改善３

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,27 +8,27 @@ import { Provider } from 'react-redux'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import * as gtag from 'src/utility/gtag'
+// import * as gtag from 'src/utility/gtag'
 import { useRouter } from 'next/router'
 
 ReactModal.setAppElement('#__next')
 
 const App = (props) => {
   const router = useRouter()
-  useEffect(() => {
-    if (!gtag.existsGaId) {
-      return
-    }
+  // useEffect(() => {
+  //   if (!gtag.existsGaId) {
+  //     return
+  //   }
 
-    const handleRouteChange = (path) => {
-      gtag.pageview(path)
-    }
+  //   const handleRouteChange = (path) => {
+  //     gtag.pageview(path)
+  //   }
 
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  //   router.events.on('routeChangeComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
   return (
     <Provider store={store}>
       <ModalProvider>
