6453aeee207012e8515bcdc4dc232855649a5b3f
hihigashi001 <hihigashi001@gmail.com>
2021-06-12 19:26:13 +0900

    error修正

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -12,12 +12,10 @@ const init = {
 export const createFirestore = async (props) => {
     const { name, mail, contents } = props;
     const createAt = dataNow()
-    console.log("load")
     try {
         await db
             .collection('mail')
             .add({ name: name, mail: mail, contents: contents, createAt: createAt });
-        console.log("成功")
     } catch {
         console.log("失敗")
     }
