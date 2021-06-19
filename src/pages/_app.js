e4e65ff4c4c830bf9838fd7bc1b3d6685e267c39
hihigashi001 <hihigashi001@gmail.com>
2021-06-19 14:07:00 +0900

    SEO対策２

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import "src/styles/globals.css";
-// import { useEffect } from "react"
 import ReactModal from "react-modal"
 import { ModalProvider } from "react-modal-hook"
 import Head from "next/head";
@@ -8,33 +7,16 @@ import { Provider } from 'react-redux'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// import * as gtag from 'src/utility/gtag'
-import { useRouter } from 'next/router'
 
 ReactModal.setAppElement('#__next')
 
 const App = (props) => {
-  const router = useRouter()
-  // useEffect(() => {
-  //   if (!gtag.existsGaId) {
-  //     return
-  //   }
-
-  //   const handleRouteChange = (path) => {
-  //     gtag.pageview(path)
-  //   }
-
-  //   router.events.on('routeChangeComplete', handleRouteChange)
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange)
-  //   }
-  // }, [router.events])
   return (
     <Provider store={store}>
       <ModalProvider>
         <Head>
           <title>あみだくじサイト | Amidakuji.net</title>
-          <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
         </Head>
         <props.Component {...props.pageProps} />
         <ToastContainer />
