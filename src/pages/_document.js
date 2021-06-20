c4c19fe8bbe4f5d63071afaf2551f7c79c6115a4
hihigashi001 <hihigashi001@gmail.com>
2021-06-20 10:16:27 +0900

    誤字修正

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="description" content="オンラインあみだくじサイトです。完全無料で使えてシンプルかつ簡単にご利用いただけます。サイト上でアミダくじを瞬時に作成してURLを共有することで公平な抽選が可能です。順番決め、幹事決め、チーム分け、グループ分け、係や役割分担、景品の抽選など使い方は工夫次第です。離れた場所にいる場合で何かを決めるのにとても役に立つサイトです。" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" href="favi16.png" sizes="16x16" type="image/png" />
