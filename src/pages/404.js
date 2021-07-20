5f8ea31f653ad47b15f9c14e6e30f38625d7f065
hihigashi001 <hihigashi001@gmail.com>
2021-07-21 06:32:18 +0900

    error画面作成

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+// layout
+import { UserLayout } from "src/layouts/UserLayout"
+
+export default function Custom404() {
+    return (
+        <UserLayout>
+          <h1>404 - Page Not Found</h1>
+        </UserLayout>
+    )
+  }
---
+++ src/pages/500.js
@@ -0,0 +1,10 @@
+// layout
+import { UserLayout } from "src/layouts/UserLayout"
+
+export default function Custom500() {
+  return (
+    <UserLayout>
+      <h1>500 - Server-side error occurred</h1>
+    </UserLayout>
+  )
+}
